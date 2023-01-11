e4cd52673e563301723d7c1ab819eb967553cd72
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2023-01-10 21:12:31 -0300

    Logs

--- Loja/src/main/java/com/microsservicos/loja/Loja/service/CompraService.java
+++ Loja/src/main/java/com/microsservicos/loja/Loja/service/CompraService.java
@@ -5,6 +5,8 @@ import com.microsservicos.loja.Loja.dto.CompraDTO;
 import com.microsservicos.loja.Loja.dto.InfoFornecedorDTO;
 import com.microsservicos.loja.Loja.dto.InfoPedidoDTO;
 import com.microsservicos.loja.Loja.model.Compra;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -14,16 +16,19 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class CompraService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(CompraService.class);
     @Autowired
     private FornecedorClient fornecedorClient;
 
     public Compra realizaCompra(CompraDTO compra) {
 
+        LOG.info("Buscando informações do fornecedor de {}", compra.getEndereco().getEstado());
+
         InfoFornecedorDTO info =fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
 
+        LOG.info("Realizando um pedido");
         InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
 
-        System.out.println(info.getEndereco());
 
         Compra compraSalva = new Compra();
         compraSalva.setPedidoId(pedido.getId());
--- fornecedor/src/main/java/com/fornecedor/fornecedor/controller/InfoController.java
+++ fornecedor/src/main/java/com/fornecedor/fornecedor/controller/InfoController.java
@@ -2,6 +2,8 @@ package com.fornecedor.fornecedor.controller;
 
 import com.fornecedor.fornecedor.model.InfoFornecedor;
 import com.fornecedor.fornecedor.service.InfoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/info")
 public class InfoController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(InfoController.class);
+
     @Autowired
     private InfoService infoService;
 
     @RequestMapping("/{estado}")
     public InfoFornecedor getInfoPorEstado(
             @PathVariable String estado){
+        LOG.info("Recibido pedido de informações de fornecedor de {}", estado );
         return infoService.getInfoPorEstado(estado);
     }
 }
